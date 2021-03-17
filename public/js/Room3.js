b8bd7d086cb2281dcd6a8753aa179d40bb4b949d
raniawehbe <rania.wehbee.22@gmail.com>
2021-03-16 22:35:52 -0400

    room2

--- public/js/Room3.js
+++ public/js/Room3.js
@@ -64,9 +64,68 @@ init: function () {
 }
 });
 
+AFRAME.registerComponent('add-ui3', {
+schema: {
+        default: '',
+        parse: AFRAME.utils.styleParser.parse
+        },
 
+init: function () {
+    
+    const el = this.el;
+    console.log('I am ready!');
 
+    el.addEventListener('click', function() {
+        
 
+        var entityEl = document.createElement('a-entity');
+        //entityEl.setAttribute('add-UI', '');
+        entityEl.setAttribute('id', "ui3");
+        entityEl.setAttribute('class', "ui3");
+        entityEl.setAttribute('position', "-29.889 29.785 -46.237");
+        entityEl.setAttribute('rotation', "0 23.975 0");
+        entityEl.setAttribute('scale', "27.674 21.022 1.125");
+        entityEl.setAttribute('geometry', {primitive: 'box', width:0.4, height:0.4, depth:0.4});
+        entityEl.setAttribute('material', "color: #995d46");
+        entityEl.setAttribute('material', { src: 'assets/hint3.png' })  
+
+        el.sceneEl.appendChild(entityEl);
+        
+
+    });
+}
+});
+
+AFRAME.registerComponent('add-ui4', {
+schema: {
+        default: '',
+        parse: AFRAME.utils.styleParser.parse
+        },
+
+init: function () {
+    
+    const el = this.el;
+    console.log('I am ready!');
+
+    el.addEventListener('click', function() {
+        
+
+        var entityEl = document.createElement('a-entity');
+        //entityEl.setAttribute('add-UI', '');
+        entityEl.setAttribute('id', "ui4");
+        entityEl.setAttribute('class', "ui4");
+        entityEl.setAttribute('position', "29.183 27.196 -11.153");
+        entityEl.setAttribute('rotation', "0 88.824 0");
+        entityEl.setAttribute('scale', "27.674 21.132 1.125");
+        entityEl.setAttribute('geometry', {primitive: 'box', width:0.4, height:0.4, depth:0.4});
+        entityEl.setAttribute('material', "color: #995d46");
+
+        el.sceneEl.appendChild(entityEl);
+        
+
+    });
+}
+});
 
 AFRAME.registerComponent('spinning-effect', {
 schema: {
@@ -102,7 +161,7 @@ Context_AF.el.addEventListener('click', function() {
     document.querySelectorAll(".ui").forEach(e => e.parentNode.removeChild(e));
     document.querySelectorAll("#panel_text1").forEach(e => e.parentNode.removeChild(e));
     cluesFound -=1;
-    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/2"});
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
 
     Context_AF.walls.setAttribute('animation__rotation', {enabled:false});
     
@@ -113,13 +172,13 @@ Context_AF.el.addEventListener('click', function() {
     Context_AF.isSpinning = true;
 
     cluesFound +=1;
-    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/2"});
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
 
     Context_AF.walls.setAttribute("animation__rotation", {enabled:true});
            
   }
 
-  if(cluesFound == 2){
+  if(cluesFound == 4){
     Context_AF.questionBtn.setAttribute('visible', "true");
     
     Context_AF.progressMeter.setAttribute('geometry', 'width: 2.5');
@@ -143,7 +202,17 @@ Context_AF.progressMeter.setAttribute('position', '-0.920 0 0.810');
 Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
 }
 
+else if(cluesFound == 2){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.25');
+  Context_AF.progressMeter.setAttribute('position', '-0.600 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
 
+else if(cluesFound == 3){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.875');
+  Context_AF.progressMeter.setAttribute('position', '-0.295 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
 
 
 });
@@ -184,7 +253,7 @@ Context_AF.el.addEventListener('click', function() {
     document.querySelectorAll(".ui2").forEach(e => e.parentNode.removeChild(e));
     document.querySelectorAll("#panel_text2").forEach(e => e.parentNode.removeChild(e));
     cluesFound -=1;
-    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/2"});
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
 
     Context_AF.walls.setAttribute('animation__rotation', {enabled:false});
   }
@@ -194,13 +263,13 @@ Context_AF.el.addEventListener('click', function() {
     Context_AF.walls.setAttribute('animation', {to:-40});
     Context_AF.isSpinning = true;
     cluesFound +=1;
-    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/2"});
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
 
     Context_AF.walls.setAttribute('animation__rotation', {enabled:true});
       
   }
 
-  if(cluesFound == 2){
+  if(cluesFound == 4){
     Context_AF.questionBtn.setAttribute('visible', "true");
     
     Context_AF.progressMeter.setAttribute('geometry', 'width: 2.5');
@@ -224,6 +293,17 @@ Context_AF.progressMeter.setAttribute('position', '-0.920 0 0.810');
 Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
 }
 
+else if(cluesFound == 2){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.25');
+  Context_AF.progressMeter.setAttribute('position', '-0.600 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
+
+else if(cluesFound == 3){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.875');
+  Context_AF.progressMeter.setAttribute('position', '-0.295 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
 
 });
 },
@@ -231,7 +311,181 @@ Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
 
 });
 
+AFRAME.registerComponent('spinning-effect3', {
+schema: {
+duration: {type: 'number', default:20000.0},  //duration is in milliseconds
+parse: AFRAME.utils.styleParser.parse
+},
+//multiple: false, //do not allow multiple instances of this component on this entity
+init: function() {
 
+//get a local reference to our entities and set some property variables
+const Context_AF = this;
+Context_AF.walls      = document.querySelector('#postcard_clue');
+Context_AF.questionBtn     = document.querySelector('#questionBtn');
+Context_AF.isSpinning = false;
+
+//let's add the basic animation to teh walls entity
+//note that it is not enabled initially
+Context_AF.walls.setAttribute('animation', {property:'position.y', to:16.0656, dur:300, easing:'linear'});
+Context_AF.walls.setAttribute('animation__rotation', {property:'rotation', to:'0 360 0', loop:true, dur:10000, easing:'linear', enabled: false});
+Context_AF.questionBtn.setAttribute('visible', "false");
+
+Context_AF.progressMeter =   document.querySelector('#progressMeter');
+Context_AF.progressMeter.setAttribute('geometry', {width: 0, depth: 0});
+
+//listen on click
+Context_AF.el.addEventListener('click', function() {
+  if (Context_AF.isSpinning === true) {
+    console.log('stop spinning');
+    Context_AF.walls.setAttribute('animation', {to:16.0656});
+    Context_AF.isSpinning = false; 
+
+    document.querySelectorAll(".ui3").forEach(e => e.parentNode.removeChild(e));
+    document.querySelectorAll("#panel_text3").forEach(e => e.parentNode.removeChild(e));
+    cluesFound -=1;
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
+
+    Context_AF.walls.setAttribute('animation__rotation', {enabled:false});
+  }
+  else {
+    console.log('spinning');
+    Context_AF.walls.setAttribute('animation', {to:17.3});
+    Context_AF.isSpinning = true;
+    cluesFound +=1;
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
+
+    Context_AF.walls.setAttribute('animation__rotation', {enabled:true});
+          
+  }
+
+  if(cluesFound == 4){
+    Context_AF.questionBtn.setAttribute('visible', "true");
+    
+    Context_AF.progressMeter.setAttribute('geometry', 'width: 2.5');
+    Context_AF.progressMeter.setAttribute('position', '0.005 0 0.810');
+    Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+  }
+
+  else{
+    Context_AF.questionBtn.setAttribute('visible', "false");
+  }
+
+
+ if(cluesFound == 0){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 0');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0');
+}
+
+else if(cluesFound == 1){
+Context_AF.progressMeter.setAttribute('geometry', 'width: 0.625');
+Context_AF.progressMeter.setAttribute('position', '-0.920 0 0.810');
+Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
+
+else if(cluesFound == 2){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.25');
+  Context_AF.progressMeter.setAttribute('position', '-0.600 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
+
+else if(cluesFound == 3){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.875');
+  Context_AF.progressMeter.setAttribute('position', '-0.295 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
+
+});
+},  
+
+});
+
+AFRAME.registerComponent('spinning-effect4', {
+schema: {
+duration: {type: 'number', default:20000.0},  //duration is in milliseconds
+parse: AFRAME.utils.styleParser.parse
+},
+//multiple: false, //do not allow multiple instances of this component on this entity
+init: function() {
+
+//get a local reference to our entities and set some property variables
+const Context_AF = this;
+Context_AF.walls      = document.querySelector('#newspaper_clue');
+Context_AF.questionBtn     = document.querySelector('#questionBtn');
+Context_AF.isSpinning = false;
+
+//let's add the basic animation to teh walls entity
+//note that it is not enabled initially
+Context_AF.walls.setAttribute('animation', {property:'position.y', to:19.28847, dur:300, easing:'linear'});
+Context_AF.walls.setAttribute('animation__rotation', {property:'rotation', to:'0 360 0', loop:true, dur:10000, easing:'linear', enabled: false});
+Context_AF.questionBtn.setAttribute('visible', "false");
+
+Context_AF.progressMeter =   document.querySelector('#progressMeter');
+Context_AF.progressMeter.setAttribute('geometry', {width: 0, depth: 0});
+
+//listen on click
+Context_AF.el.addEventListener('click', function() {
+  if (Context_AF.isSpinning === true) {
+    console.log('stop spinning');
+    Context_AF.walls.setAttribute('animation', {to:19.28847});
+    Context_AF.isSpinning = false; 
+
+    document.querySelectorAll(".ui4").forEach(e => e.parentNode.removeChild(e));
+    cluesFound -=1;
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
+
+    Context_AF.walls.setAttribute('animation__rotation', {enabled:false});
+  }
+
+  else {
+    console.log('spinning');
+    Context_AF.walls.setAttribute('animation', {to:20.4});
+    Context_AF.isSpinning = true;
+    cluesFound +=1;
+    document.querySelector('#clues_found').setAttribute('text', {value:"Clues Found: " + cluesFound + "/4"});
+
+    Context_AF.walls.setAttribute('animation__rotation', {enabled:true});          
+  }
+
+  if(cluesFound == 4){
+    Context_AF.questionBtn.setAttribute('visible', "true");
+    
+    Context_AF.progressMeter.setAttribute('geometry', 'width: 2.5');
+    Context_AF.progressMeter.setAttribute('position', '0.005 0 0.810');
+    Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+  }
+
+  else{
+    Context_AF.questionBtn.setAttribute('visible', "false");
+  }
+
+
+ if(cluesFound == 0){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 0');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0');
+}
+
+else if(cluesFound == 1){
+Context_AF.progressMeter.setAttribute('geometry', 'width: 0.625');
+Context_AF.progressMeter.setAttribute('position', '-0.920 0 0.810');
+Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
+
+else if(cluesFound == 2){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.25');
+  Context_AF.progressMeter.setAttribute('position', '-0.600 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
+
+else if(cluesFound == 3){
+  Context_AF.progressMeter.setAttribute('geometry', 'width: 1.875');
+  Context_AF.progressMeter.setAttribute('position', '-0.295 0 0.810');
+  Context_AF.progressMeter.setAttribute('geometry', 'depth: 0.24');
+}
+
+});
+},
+});
 
 
 AFRAME.registerComponent('false-clue', {
@@ -325,7 +579,7 @@ AFRAME.registerComponent('visibility', {
 
             Context_AF.el.addEventListener('click', function() {
 
-                if(cluesFound == 2){
+                if(cluesFound == 4){
                     
                     Context_AF.question.setAttribute('visible', "true");
                     Context_AF.question1.setAttribute('visible', "true");
